b9c458e5d77aca6d744857bed58f72fdf36fda2e
rei <rei.balla@rayonit.com>
2019-03-30 13:36:59 +0100

    #4 Done

---
+++ microservices/images-service/src/main/java/azul/project/imagesapplication/storage/StorageException.java
@@ -0,0 +1,12 @@
+package azul.project.imagesapplication.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
