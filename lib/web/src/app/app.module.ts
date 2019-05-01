2b14740761165ff3a6b0cc672dc8d61d5ea1a571
rei <rei.balla@rayonit.com>
2019-05-01 16:57:40 +0200

    ...

--- lib/web/src/app/app.module.ts
+++ lib/web/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
 import {BaseLayoutModule} from './base-layout/base-layout.module';
+import {PagesModule} from './pages/pages.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {BaseLayoutModule} from './base-layout/base-layout.module';
   imports: [
     BrowserModule,
     SharedModule,
-    BaseLayoutModule
+    BaseLayoutModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
